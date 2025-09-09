a523f116c6d7a2a824f497917305d3caab694d08
api-69 <markapilado.scc@gmail.com>
2025-09-10 07:22:31 +0800

    Create kupal.java

---
+++ kupal.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		int age=18;
+		String name="Java Learner";
+		float height=5.7f;
+	    double weight=65.5;
+	    char grade='A';
+	    boolean isPassed=true;
+	    
+	    System.out.println("Name:" + name);
+	    System.out.println("Age:" + age);
+	    System.out.println("Height:" + height);
+	    System.out.println("Weight:" + weight);
+	    System.out.println("Grade:" + grade);
+	    System.out.println("Passed:" + isPassed);
+	}
+}
